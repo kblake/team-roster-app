8114c40826d4b15538fe968b9cd78a1053c38965
Karmen Blake <kblake@magnersanborn.com>
2013-10-12 20:09:16 -0700

    initial commit

---
+++ roster.js
@@ -0,0 +1,21 @@
+var app = angular.module('rosterApp', ['ngSanitize', 'ngAnimate']);
+
+app.controller('rosterCtrl', ['$scope', function ($scope) {
+  $scope.newPlayer = {};
+
+  $scope.players = [
+    {"firstname":"John", "lastname":"Doe", "age":"15", "position":"center", "email":"john@doe.com"},
+    {"firstname":"Jack", "lastname":"Black", "age":"16", "position":"point guard", "email":"jack@black.com"},
+    {"firstname":"Randall", "lastname":"Good", "age":"15", "position":"forward", "email":"r@good.com"}
+  ];
+
+  $scope.addPlayer = function() {
+    $scope.players.push($scope.newPlayer);
+    $scope.newPlayer = {};
+  };
+
+  $scope.removePlayer = function(playerIndex) {
+    $scope.players.splice(playerIndex, 1);
+  };
+  
+}]);
